4538f0086b76d8313ddf9cd60b995cdc3a1c4431
Myintmoethwe <myintmoethwe5@gmail.com>
2026-09-08 09:08:50 +0630

    fix error

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const app = require("./app.js");
 const { PORT } = require("./config/config.js");
 
+sequelize.sync({ alter: true })
+  .then(() => console.log('Database synced successfully'))
+  .catch((err) => console.log('Error syncing database:', err));
+
 module.exports = app;
